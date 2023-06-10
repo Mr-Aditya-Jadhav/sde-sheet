5db1ee247b7c1460d84badff8bc4acc42d310c2c
Aditya Jadhav <36039636+Mr-Aditya-Jadhav@users.noreply.github.com>
2023-06-10 17:33:15 +0530

    Create consecutiveones.cpp

---
+++ consecutiveones.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/max-consecutive-ones/submissions/968001064/
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int maxone = 0; 
+        int count = 0;
+        for(int i = 0 ; i < nums.size(); i++){
+            if(nums[i] == 1){
+                count++;
+            }
+            else{
+                count = 0;
+            }
+
+            maxone = max(count, maxone);
+        }
+
+        return maxone;
+    }
+};
