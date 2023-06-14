2fba51fa8c2406ae1701ce70fbfe906d7bf6b0cd
Aditya Jadhav <36039636+Mr-Aditya-Jadhav@users.noreply.github.com>
2023-06-14 23:50:42 +0530

    Create permutaion.cpp

---
+++ permutaion.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/permutation-sequence/description/
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact = 1;
+        vector<int>nums;
+        for(int i = 1 ; i <= n; i++){
+            nums.push_back(i);
+            fact = fact * i;
+        }
+        fact = fact/n;
+        string ans = "";
+        k--;
+
+        while(true){
+            ans += to_string(nums[k / fact]);
+            nums.erase( nums.begin() + k / fact);
+            if(nums.size() == 0){
+                break;
+            }
+            k = k % fact;
+            fact = fact / nums.size();
+        }
+
+        return ans;
+
+    }
+
+     
+};
